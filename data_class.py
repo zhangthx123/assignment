0114896ec087876c3d882e78e816a0650ab318ff
zhangthx123 <53855424+zhangthx123@users.noreply.github.com>
2025-11-30 17:36:06 +0800

    Add files via upload

---
+++ data_class.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+  
+class Truck:
+    V_truck = []
+    W_truck = []
+    Cost_truck = []
+    Index_truck = []
+    Order_truck = []  # 车辆装载的订单
+    T_truck = []
+    Rv_truck = []
+    Rw_truck = []
+    Order_last_truck = []  # 车辆已分配订单中按序定顺序排序的最后一个订单的index
+    
+    
+class Order: 
+    V_order = [] 
+    W_order = []  
+    Time_earliest = []
+    Time_latest = []
+    Index_order = []
+    Seq_order = []
+    
+
+
